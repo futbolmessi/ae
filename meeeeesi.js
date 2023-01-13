0e389e221ee0d386686abc67be17d51996ea9778
mini messi <98719161+futbolmessi@users.noreply.github.com>
2023-01-12 20:56:37 -0600

    aaa
    eeee

---
+++ meeeeesi.js
@@ -0,0 +1,14 @@
+var firebaseConfig = {
+    apiKey: "AIzaSyD-e7xqaLF2AUgOeY6UlY3giOwp6g96nOI",
+    authDomain: "sfpba-240fd.firebaseapp.com",
+    databaseURL: "https://sfpba-240fd-default-rtdb.firebaseio.com",
+    projectId: "sfpba-240fd",
+    storageBucket: "sfpba-240fd.appspot.com",
+    messagingSenderId: "1098961656742",
+    appId: "1:1098961656742:web:d2f85cf1551c02f23f8d67",
+    measurementId: "G-9BQBBYQJM9"
+  };
+  
+  firebase.initializeApp(firebaseConfig);
+   
+  user_name=localStorage.getItem("user_name");
